905ac27412a576597f814e826e72862512b431ab
Conrad Muan <con.muan@gmail.com>
2014-09-27 17:51:36 -0700

    added tweets to a modal

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -2,4 +2,21 @@
   $('.clownscore').hover(function(){
     document.getElementById('faffel').play();
   });
+
+  var tweetHTML;
+
+  $('.open-modal').on('click', function(e){
+    e.preventDefault();
+    tweetHTML = $(this).parents('.tweet').find('.tweet-text').clone();
+    $('#tweet-modal').modal('show');
+  });
+
+  $('#tweet-modal').on('show.bs.modal', function(e){
+    $(this).find('.modal-body').append(tweetHTML);
+  });
+
+  $('#tweet-modal').on('hide.bs.modal', function(e){
+    $(this).find('.modal-body').empty();
+  });
+
 }());
